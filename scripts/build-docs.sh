628275fe3376b986e57971b6c5171f65b8f77d06
Nate Marti <nate_marti@sil.org>
2024-05-13 21:02:47 +0100

    add feature roadmap to docs

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -2,11 +2,13 @@
 
 script_dir="$(dirname "$0")"
 repo_dir="$(dirname "$script_dir")"
-# cd "${repo_dir}/sphinx" || exit 1
+cd "${repo_dir}/sphinx" || exit 1
 
 if [[ $1 == '-c' || $1 == '--clean' ]]; then
     # clean before building
-    rm -f "${repo_dir}/docs"*
+    make clean
+    # rm -f "${repo_dir}/docs"*
 fi
 
-sphinx-build "${repo_dir}/sphinx/source" "${repo_dir}/docs"
+make html
+# sphinx-build "${repo_dir}/sphinx/source" "${repo_dir}/docs"
