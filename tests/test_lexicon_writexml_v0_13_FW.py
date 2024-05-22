f0db4c9f86e993e71d24db5014377318333d3b8e
Nate Marti <nate_marti@sil.org>
2024-05-22 12:51:38 +0100

    add more unittests

--- tests/test_lexicon_writexml_v0_13_FW.py
+++ tests/test_lexicon_writexml_v0_13_FW.py
@@ -12,39 +12,23 @@ from lift_utils import utils
 LIFT_VERSION = '0.13'
 
 
-@unittest.skip('method not ready')
-class TestExtensible(unittest.TestCase):
+class TestEtymology(unittest.TestCase):
     def test_xml(self):
+        config.LIFT_VERSION = LIFT_VERSION
         obj = lexicon.Etymology()
         obj.type = datatypes.Key("Etype")
         obj.source = "Esource"
-        obj.form = base.Form()
         obj.glosses = [
             base.Gloss(),
             base.Gloss(),
         ]
-        obj.date_created = datatypes.DateTime("0000-00-00")
-        obj.date_modified = datatypes.DateTime("0000-00-00")
-        obj.fields = [
-            base.Field(),
-            base.Field(),
-            base.Trait(),
-            base.Trait(),
-            base.Annotation(),
-            base.Annotation(),
-        ]
+        obj.form = base.Form()
 
         xml = f"""
-        <etymology type="Etype" source="Esource" dateCreated="{obj.date_created}" dateModified="{obj.date_modified}">
+        <etymology type="Etype" source="Esource">
+            <gloss/>
+            <gloss/>
             <form/>
-            <gloss/>
-            <gloss/>
-            <field/>
-            <field/>
-            <trait/>
-            <trait/>
-            <annotation/>
-            <annotation/>
         </etymology>
         """
         xml_tree = utils.xmlstring_to_etree(xml)
@@ -54,42 +38,96 @@ class TestExtensible(unittest.TestCase):
         )
 
 
-@unittest.skip('not yet implemented')
-class TestNote(unittest.TestCase):
+class TestExtensible(unittest.TestCase):
     def test_xml(self):
-        obj = lexicon.Note()
+        config.LIFT_VERSION = LIFT_VERSION
+        obj = lexicon.Etymology()
         obj.type = datatypes.Key("Etype")
         obj.source = "Esource"
-        obj.form = base.Form()
         obj.glosses = [
             base.Gloss(),
             base.Gloss(),
         ]
+        obj.form = base.Form()
         obj.date_created = datatypes.DateTime("0000-00-00")
         obj.date_modified = datatypes.DateTime("0000-00-00")
         obj.fields = [
             base.Field(),
             base.Field(),
+        ]
+        obj.traits = [
             base.Trait(),
             base.Trait(),
+        ]
+        obj.annotations = [
             base.Annotation(),
             base.Annotation(),
         ]
 
         xml = f"""
-        <etymology type="Etype" source="Esource" dateCreated="{obj.date_created}" dateModified="{obj.date_modified}">
+        <etymology dateCreated="{obj.date_created}" dateModified="{obj.date_modified}" type="Etype" source="Esource">
+            <field/>
+            <field/>
+            <trait/>
+            <trait/>
+            <annotation/>
+            <annotation/>
+            <gloss/>
+            <gloss/>
             <form/>
-            <gloss/>
-            <gloss/>
-            <field/>
-            <field/>
-            <trait/>
-            <trait/>
-            <annotation/>
-            <annotation/>
         </etymology>
         """
         xml_tree = utils.xmlstring_to_etree(xml)
+        # print(etree.tostring(obj._to_xml_tree(), pretty_print=True).decode())
+        # print(etree.tostring(xml_tree, pretty_print=True).decode())
+        self.assertEqual(
+            etree.tostring(obj._to_xml_tree(), pretty_print=True),
+            etree.tostring(xml_tree, pretty_print=True)
+        )
+
+
+class TestNote(unittest.TestCase):
+    def test_xml(self):
+        config.LIFT_VERSION = LIFT_VERSION
+        obj = lexicon.Note()
+        obj.type = datatypes.Key("note-type")
+        for p in obj.props.attributes:
+            print(p.name)
+
+        xml = f"""
+        <note type="note-type"/>
+        """
+        xml_tree = utils.xmlstring_to_etree(xml)
+        self.assertEqual(
+            etree.tostring(obj._to_xml_tree(), pretty_print=True),
+            etree.tostring(xml_tree, pretty_print=True)
+        )
+
+
+class TestPhonetic(unittest.TestCase):
+    def test_xml(self):
+        config.LIFT_VERSION = LIFT_VERSION
+        obj = lexicon.Phonetic()
+        obj.medias = [
+            base.URLRef(),
+            base.URLRef(),
+        ]
+        obj.forms = [
+            base.Span(),
+            base.Span(),
+        ]
+
+        xml = f"""
+        <phonetic>
+            <media/>
+            <media/>
+            <form/>
+            <form/>
+        </phonetic>
+        """
+        xml_tree = utils.xmlstring_to_etree(xml)
+        # print(etree.tostring(obj._to_xml_tree(), pretty_print=True))
+        # print(etree.tostring(xml_tree, pretty_print=True))
         self.assertEqual(
             etree.tostring(obj._to_xml_tree(), pretty_print=True),
             etree.tostring(xml_tree, pretty_print=True)
