c98e3050d83033dfcc4711b26b07d76301418cb1
Nate Marti <nate_marti@sil.org>
2024-06-11 05:16:25 +0100

    fix formatting

--- scripts/run-profiler.sh
+++ scripts/run-profiler.sh
@@ -5,6 +5,7 @@ scripts_dir="$(dirname "$0")"
 repo_dir="$(dirname "$scripts_dir")"
 cd "$repo_dir" || exit 1
 if [[ -z $VIRTUAL_ENV ]]; then
+    # shellcheck disable=SC1091
     source ./env/bin/activate
 fi
 
