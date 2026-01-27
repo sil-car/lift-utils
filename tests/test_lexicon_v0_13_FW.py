9fb5c8f85f88b1f31bc9ee7990089f7af5912098
Nate Marti <nate_marti@sil.org>
2026-01-27 08:44:48 -0600

    lint: mostly auto-lint fixes

--- tests/test_lexicon_v0_13_FW.py
+++ tests/test_lexicon_v0_13_FW.py
@@ -1,10 +1,10 @@
 import unittest
+
 from lxml import etree
 
-from . import DATA_PATH
-from . import LEXICON
-
 from lift_utils.utils import xmlfile_to_etree
+
+from . import DATA_PATH, LEXICON
 
 LIFT_GOOD = DATA_PATH / "sango" / "sango.lift"
 
@@ -16,14 +16,8 @@ class TestParse(unittest.TestCase):
     def test_lexicon_find_writing_systems(self):
         self.lexicon.vernacular_writing_systems.sort()
         self.lexicon.analysis_writing_systems.sort()
-        self.assertEqual(
-            self.lexicon.vernacular_writing_systems,
-            ['sg', 'sg-fonipa']
-        )
-        self.assertEqual(
-            self.lexicon.analysis_writing_systems,
-            ['de', 'en', 'fr']
-        )
+        self.assertEqual(self.lexicon.vernacular_writing_systems, ["sg", "sg-fonipa"])
+        self.assertEqual(self.lexicon.analysis_writing_systems, ["de", "en", "fr"])
 
     def test_lexicon_header_field_items(self):
         self.assertTrue(len(self.lexicon.header.fields.field_items) == 8)
@@ -58,31 +52,47 @@ class TestReadWrite(unittest.TestCase):
         infile = LIFT_GOOD
         outfile = DATA_PATH / "sango_out.lift"
         LEXICON.to_lift(outfile)
-        xml_in = etree.tostring(
-            xmlfile_to_etree(infile),
-            encoding='UTF-8',
-            pretty_print=True,
-            xml_declaration=True
-        ).decode().rstrip()
-        xml_out = etree.tostring(
-            xmlfile_to_etree(outfile),
-            encoding='UTF-8',
-            pretty_print=True,
-            xml_declaration=True
-        ).decode().rstrip()
+        xml_in = (
+            etree.tostring(
+                xmlfile_to_etree(infile),
+                encoding="UTF-8",
+                pretty_print=True,
+                xml_declaration=True,
+            )
+            .decode()
+            .rstrip()
+        )
+        xml_out = (
+            etree.tostring(
+                xmlfile_to_etree(outfile),
+                encoding="UTF-8",
+                pretty_print=True,
+                xml_declaration=True,
+            )
+            .decode()
+            .rstrip()
+        )
 
-        ranges_xml_in = etree.tostring(
-            xmlfile_to_etree(infile.with_suffix('.lift-ranges')),
-            encoding='UTF-8',
-            pretty_print=True,
-            xml_declaration=True
-        ).decode().rstrip()
-        ranges_xml_out = etree.tostring(
-            xmlfile_to_etree(outfile.with_suffix('.lift-ranges')),
-            encoding='UTF-8',
-            pretty_print=True,
-            xml_declaration=True
-        ).decode().rstrip()
+        ranges_xml_in = (
+            etree.tostring(
+                xmlfile_to_etree(infile.with_suffix(".lift-ranges")),
+                encoding="UTF-8",
+                pretty_print=True,
+                xml_declaration=True,
+            )
+            .decode()
+            .rstrip()
+        )
+        ranges_xml_out = (
+            etree.tostring(
+                xmlfile_to_etree(outfile.with_suffix(".lift-ranges")),
+                encoding="UTF-8",
+                pretty_print=True,
+                xml_declaration=True,
+            )
+            .decode()
+            .rstrip()
+        )
         # xml_in = etree.canonicalize(
         #     xmlfile_to_etree(infile),
         #     strip_text=True
@@ -99,16 +109,8 @@ class TestReadWrite(unittest.TestCase):
         delta = 0.05
         # print(len(xml_out)/len(xml_in))
         # print(len(ranges_xml_out)/len(ranges_xml_in))
-        self.assertAlmostEqual(
-            1,
-            len(xml_out)/len(xml_in),
-            delta=delta
-        )
-        self.assertAlmostEqual(
-            1,
-            len(ranges_xml_out)/len(ranges_xml_in),
-            delta=delta
-        )
+        self.assertAlmostEqual(1, len(xml_out) / len(xml_in), delta=delta)
+        self.assertAlmostEqual(1, len(ranges_xml_out) / len(ranges_xml_in), delta=delta)
 
 
 class TestFind(unittest.TestCase):
@@ -116,54 +118,41 @@ class TestFind(unittest.TestCase):
         self.lexicon = LEXICON
 
     def test_find_all_definition(self):
-        self.assertEqual(
-            len(self.lexicon.find_all(
-                "husband",
-                field="definition"
-            )), 2)
+        self.assertEqual(len(self.lexicon.find_all("husband", field="definition")), 2)
 
     def test_find_all_gloss(self):
         self.assertEqual(len(self.lexicon.find_all("head")), 20)
 
     def test_find_all_grammatical_info(self):
         self.assertEqual(
-            len(self.lexicon.find_all("Nom", field="grammatical-info")),
-            2381
+            len(self.lexicon.find_all("Nom", field="grammatical-info")), 2381
         )
 
     def test_find_all_header_field_contains(self):
         self.assertEqual(len(self.lexicon.find_all("1021", field="CAWL")), 1)
 
     def test_find_all_header_field_exact(self):
-        self.assertEqual(len(self.lexicon.find_all(
-            "1056",
-            field="CAWL",
-            match_type="exact"
-        )), 1)
+        self.assertEqual(
+            len(self.lexicon.find_all("1056", field="CAWL", match_type="exact")), 1
+        )
 
     def test_find_definition_contains(self):
         self.assertEqual(
-            str(self.lexicon.find("man", field="definition").id),
-            "f6c860ea-80ff-431a-a5dd-9469b315f40c"
+            str(self.lexicon.find("husbandry", field="definition").id),
+            # "f6c860ea-80ff-431a-a5dd-9469b315f40c",  # id for "man"; wrong?
+            "ebcf4d79-6bb9-4d33-9f31-cdc03355a718",  # id for "husbandry"
         )
 
     def test_find_gloss_contains(self):
         self.assertEqual(
-            str(self.lexicon.find("house").id),
-            "086e06e3-a5f6-4c54-805a-4a1adcd51d4f"
+            str(self.lexicon.find("house").id), "086e06e3-a5f6-4c54-805a-4a1adcd51d4f"
         )
 
     def test_find_header_field_contains(self):
         self.assertEqual(
             str(self.lexicon.find("i-1056", field="CAWL").id),
-            "25222115-e109-4704-8e82-d7cd523c8cbd"
+            "25222115-e109-4704-8e82-d7cd523c8cbd",
         )
 
     def test_find_header_field_exact(self):
-        self.assertIsNone(
-            self.lexicon.find(
-                "056",
-                field="CAWL",
-                match_type="exact"
-            )
-        )
+        self.assertIsNone(self.lexicon.find("056", field="CAWL", match_type="exact"))
