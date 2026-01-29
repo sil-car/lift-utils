0c78aea14e3518de00d983d84a6641a421690dc7
Nate Marti <nate_marti@sil.org>
2026-01-29 13:52:02 -0600

    fix: don't automatically load sangol.lift Lexicon in tests.__init__

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,7 +3,5 @@ from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).parents[1] / "src"))
 
-from lift_utils import Lexicon  # noqa: E402
-
 DATA_PATH = Path(__file__).parent / "data"
-LEXICON = Lexicon(DATA_PATH / "sango" / "sango.lift")
+SANGO_LIFT = DATA_PATH / "sango" / "sango.lift"
